0e2c1de255c62f414b7e5bbeb5f44c02a4e03660
Josh <mesakmegerem@gmail.com>
2024-10-03 23:41:50 +0300

    Fix creating superuser

--- giveaway/models.py
+++ giveaway/models.py
@@ -10,32 +10,39 @@ PHONE_REGEX = RegexValidator(
     )
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, phone_number, **kwargs):
+    def create_user(self, phone_number, password=None, **kwargs):
         if not phone_number:
             raise ValueError("User must have phone number")
         user = self.model(phone_number=phone_number, **kwargs)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
-    
-    def create_superuser(self, phone_number, **kwargs):
-        user = self.create_user(phone_number, **kwargs)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
 
-        return user
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        
+        return self.create_user(phone_number, password=password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100, unique=True, null=True)
     phone_number = models.CharField(validators=[PHONE_REGEX], max_length=13, unique=True)
-    telegram_id = models.IntegerField(unique=True)
+    telegram_id = models.IntegerField(unique=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     is_taker = models.BooleanField(default=False)
     is_gifter = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserAccountManager()
 
