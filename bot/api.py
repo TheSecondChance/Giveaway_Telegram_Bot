9cd056abb4e7bab971b8167bae6eba8a8d42d950
Josh <mesakmegerem@gmail.com>
2024-10-07 23:09:02 +0300

    annotate functions

--- bot/api.py
+++ bot/api.py
@@ -1,13 +1,22 @@
 import requests
 from rest_framework import status
 from rest_framework.response import Response
+from typing import Optional, Dict, Any
 
 BASE_URL = 'http://127.0.0.1:8000/'
 
 def status(status):
     pass
 
-def get_user(telegram_id: int):
+def get_user(telegram_id: int) -> Optional[Dict[str, Any]]:
+    """Get user by telegram id.
+    
+    Args:
+        telegram_id (int): The user's Telegram ID.
+    
+    Returns:
+        Optional[Dict[str, Any]]: The user data in a dictionary if found, None otherwise.
+    """
     params = {
         'telegram_id': telegram_id
     }
@@ -19,23 +28,40 @@ def get_user(telegram_id: int):
     else:
         return None
 
-def create_user(created_data: dict):
-    """Create User required: telegram_id"""
+def create_user(created_data: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+    """Create a new user by sending a POST request.
+
+    Args:
+        created_data (Dict[str, Any]): A dictionary containing the user data to create.
+
+    Returns:
+        Optional[Dict[str, Any]]: The created user data if the request is successful,
+        or an error message if something goes wrong.
+    """
     response = requests.post(BASE_URL + 'api/create-user/', json=created_data)
 
     if response.status_code == 201:
-        return response.json()
+        return response.json() 
     elif response.status_code == 404:
-        return Response({"detail": "Error not Found"},
-                                status=status.HTTP_404_NOT_FOUND)
+        return {"detail": "Error not Found", "status": 404}
     elif response.status_code == 400:
-        return Response({"detail": "Bad request"},
-                        status=status.HTTP_400_BAD_REQUEST)
+        return {"detail": "Bad request", "status": 400}
     else:
-        print("Something went wrong in create", response.status_code, response.text)
+        print(f"Something went wrong in create: {response.status_code} - {response.text}")
+        return None
 
-def update_user(telegram_id: int, updated_data: dict):
-    """update user by telegram id"""
+def update_user(telegram_id: int, updated_data: dict) -> Optional[Dict[str, Any]]:
+    """Update user by send PUT request.
+
+    Args:
+        telegram_id (int): The user's Telegram ID.
+        updated_data (dict): A dictionary containing the user data to update.
+
+    Returns:
+        Returns:
+        Optional[Dict[str, any]]: The updated user data if the request is successful,
+        or an error message if something goes wrong.
+    """
 
     params = {
         'telegram_id': telegram_id
