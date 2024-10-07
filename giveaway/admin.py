6fca3ebccfcea536a246d2d3732513d0dbda3cdf
Josh <mesakmegerem@gmail.com>
2024-10-07 10:06:08 +0300

    Add simple admin

--- giveaway/admin.py
+++ giveaway/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Gifter, Taker, User, Question, Answer
 
-# Register your models here.
+
+admin.site.register(Gifter)
+
+admin.site.register(Taker)
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Answer)
