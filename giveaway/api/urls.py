7096cb78f13dd71b6baa97ee807eb96f0428a935
Josh <mesakmegerem@gmail.com>
2024-10-07 10:12:57 +0300

    Add endpoint using DefaultRouter

--- giveaway/api/urls.py
+++ giveaway/api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import hook
+from rest_framework.routers import DefaultRouter
+from .views import hook, UserCreateViewSet, UserTelegramIdViewSet
 
+
+router = DefaultRouter()
+router.register(r'create-user', UserCreateViewSet)
+# router.register(r'get-telegram-id', UserTelegramIdViewSet, basename='get-by-tg-id')
 
 urlpatterns = [
     path("web-hook", hook, name="web-hook"),
+    path("get-telegram-id/", UserTelegramIdViewSet.as_view(), name="get-by-tg-id"),
 ]
+
+urlpatterns += router.urls
