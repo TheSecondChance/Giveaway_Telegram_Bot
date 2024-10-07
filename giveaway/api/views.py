9a0e2d9bac8511c91310b4c205ad448d1809bea0
Josh <mesakmegerem@gmail.com>
2024-10-07 10:14:45 +0300

    Add User ViewSet

--- giveaway/api/views.py
+++ giveaway/api/views.py
@@ -1,7 +1,15 @@
 import json, telebot
-from giveaway.bot import bot
+from bot.main import bot
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView
+
+from .serializers import UserSerializer, UserTelegramIdSerializer
+from giveaway.models import User
 
 @csrf_exempt
 def hook(request):
@@ -16,6 +24,46 @@ def hook(request):
     bot.process_new_updates([update])
     return JsonResponse({"status": "200"}, safe=False)
 
+class UserCreateViewSet(CreateModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserTelegramIdViewSet(RetrieveAPIView, UpdateAPIView):
+    serializer_class = UserTelegramIdSerializer
+
+    def get(self, request, *args, **kwargs):
+        telegram_id = request.query_params.get('telegram_id')
+
+        if telegram_id:
+            user = User.objects.filter(telegram_id=telegram_id).first()
+
+            if user:
+                serializer = self.get_serializer(user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"detail": "No user found with the provided telegram_id"},
+                                status=status.HTTP_404_NOT_FOUND)
+
+        return Response({"detail": "telegram_id query param is required"},
+                        status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, *args, **kwargs):
+        telegram_id = request.query_params.get('telegram_id')
+        if telegram_id:
+            user = User.objects.filter(telegram_id=telegram_id).first()
+            if user:
+                serializer = self.get_serializer(instance=user, data=request.data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response({"detail": "No user found with the provided telegram_id"},
+                                status=status.HTTP_404_NOT_FOUND)
+
+        return Response({"detail": "telegram_id query param is required"},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    
 
 # TELEGRAM_BOT_WEBHOOK_URL = "https://bcd68b0417f6aeef18b0fe38d16faa40.serveo.net/account/web-hook"
 
