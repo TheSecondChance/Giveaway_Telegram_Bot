5ba17a4643d4bf6e97ab9f9b8514e97ba5cc9079
Josh <mesakmegerem@gmail.com>
2024-10-15 20:04:07 +0300

    Add update and get function for handle after_question

--- bot/api.py
+++ bot/api.py
@@ -173,3 +173,56 @@ def user_exists(user_id: int, bot_send) -> bool:
         else:
             logging.error(f"Unexpected error for user {user_id}: {e}")
         return False
+
+def get_after_answer(telegram_id: int) -> Optional[Dict[str, Any]]:
+    """Get user by telegram id.
+    
+    Args:
+        telegram_id (int): The user's Telegram ID.
+    
+    Returns:
+        Optional[Dict[str, Any]]: The user data in a dictionary if found, None otherwise.
+    """
+    params = {
+        'telegram_id': telegram_id
+    }
+
+    response = requests.get(BASE_URL + "api/after/", params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def update_after_answer(telegram_id: int, question_code: int, updated_data: dict) -> Optional[Dict[str, Any]]:
+    """Update user by send PUT request.
+
+    Args:
+        telegram_id (int): The user's Telegram ID.
+        updated_data (dict): A dictionary containing the user data to update.
+
+    Returns:
+        Returns:
+        Optional[Dict[str, any]]: The updated user data if the request is successful,
+        or an error message if something goes wrong.
+    """
+
+    params = {
+        'telegram_id': telegram_id,
+        'question_code': question_code
+    }
+    response = requests.put(
+        url=BASE_URL + 'api/after/',
+        params=params,
+        json=updated_data
+    )
+    if response.status_code == 200:
+        return response.json()
+    elif response.status_code == 404:
+        return Response({"detail": "No user found with the provided telegram_id"},
+                        status=status.HTTP_400_BAD_REQUEST)
+    elif response.status_code == 400:
+        return Response({"detail": "Bad request"},
+                        status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({"detail": "something wrong"},  response.status_code)
