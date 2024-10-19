62b11f07dc1c8ea7549109727c4c2667c157f1fe
Josh <mesakmegerem@gmail.com>
2024-10-19 16:53:39 +0300

    Remove contact_share functions

--- bot/main.py
+++ bot/main.py
@@ -50,7 +50,6 @@ def handle_language_selection(call: types.CallbackQuery):
             "language": call.data
         }
         create_user(created_data=data)
-        response_user = get_user(telegram_id=user.id)
         language = call.data
 
     msg = bot.send_message(call.message.chat.id, text=(
@@ -61,75 +60,22 @@ def handle_language_selection(call: types.CallbackQuery):
         call.id,
         (f"The Language Preference is set to {language.capitalize()}", language),
     )
-
-    response_user = get_user(telegram_id=user.id)
     msg.from_user = user
-    is_not_phone_exisit = response_user.get('phone_number', None)
-
-    if is_not_phone_exisit == None:
-        request_contact_share(msg)
-    else:
-        start(msg)
-
-def request_contact_share(message):
-    keyboard = types.ReplyKeyboardMarkup(
-        row_width=1,
-        resize_keyboard=True,
-        one_time_keyboard=True
-    )
-    share = "📲 Share your contact to get started!"
-    btn1 = types.KeyboardButton(("Share Contact 👩‍🎓"), request_contact=True)
-    keyboard.add(btn1)
-
-    with open('./Assets/share_contact.png', 'rb') as photo:
-        response_message = bot.send_photo(
-            message.chat.id, photo, caption=f"{share}",
-            reply_markup=keyboard
-        )
-    try:
-        bot.delete_message(message.chat.id, message.message_id)
-    except telebot.apihelper.ApiTelegramException as e:
-        print(f"Failed share to delete message {message.message_id}: {e}")
-
-    threading.Thread(target=delete_message_after_delay, args=(message.chat.id, response_message.message_id, 10)).start()
-
-@bot.message_handler(content_types=["contact"])
-def handle_shared_contact(message: types.Message):
-    try:
-        contact = message.contact
-        updated_data = {
-            "phone_number": contact.phone_number,
-            "first_name": contact.first_name,
-            "last_name": contact.last_name,
-            "is_taker": False,
-            "is_gifter": False,
-            "is_active": True
-        }
-        update_user(telegram_id=contact.user_id, updated_data=updated_data)
-    except Exception as e:
-        print(f"Failed to save contact information to the database: {e}")
-    response_message = bot.send_message(
-        chat_id=message.chat.id,
-        text=("Contact Received ✅")
-    )
-    threading.Thread(target=delete_message_after_delay, args=(
-        message.chat.id, response_message.message_id, 1)).start()
-    
-    start(message)
+    start(msg)
 
 @bot.message_handler(commands=['start', 'restart']) 
 def start(message):
     user = get_user(telegram_id=message.chat.id)
     if user != None:
         language = user.get('language', None)
-        is_phone_exisit = user.get('phone_number')
+        is_telegram_id = user.get('telegram_id')
         is_giver = user.get('is_gifter')
         is_taker = user.get('is_taker')
         if is_giver == True:
             Giver_welcome(message, userId=message.chat.id)
         elif is_taker == True:
             taker_welcome(message, userId=message.chat.id) 
-        elif is_phone_exisit is not None:
+        elif is_telegram_id is not None:
             chose_role(language, message)
         else:
             select_language(message)
@@ -159,10 +105,10 @@ def Giver_welcome(message, userId=None):
     if userId:
         user = get_user(telegram_id=userId)
     else:
-        user = get_user(telegram_id=message.from_user.id)
+        user = get_user(telegram_id=message.chat.id)
     inline_markup = types.InlineKeyboardMarkup(row_width=2)
     language = user.get('language', None)
-    welcome_msg = _(start_msg, language)
+    msg = _(start_msg, language)
     btn1 = types.InlineKeyboardButton(_("Question Code 🧑‍💻", language), callback_data="question_code")
     btn2 = types.InlineKeyboardButton(_("Result 🧧", language), callback_data="result_giver")
     btn5 = types.InlineKeyboardButton(_("Insert Answer ✏️", language), callback_data="insert_answer")
@@ -172,9 +118,8 @@ def Giver_welcome(message, userId=None):
     inline_markup.row(btn5, btn3)
     inline_markup.row(btn4)
 
-
-    bot.send_photo(message.chat.id, WELCOME_IMAGE, caption=welcome_msg, reply_markup=inline_markup)
-
+    with open('./Assets/welcome_dr.png', 'rb') as photo:
+        bot.send_photo(message.chat.id, photo, caption=msg, reply_markup=inline_markup)
     try:
         bot.delete_message(message.chat.id, message.message_id)
     except telebot.apihelper.ApiTelegramException as e:
